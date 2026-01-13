8d8375aefafff8971edb8f46fe7191429870ee6a
vikumkanishka <vikumkanishka14@gmail.com>
2026-01-13 14:41:06 +0530

    Add customer management UI, styles, and migration configuration

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import edu.icet.service.CustomerService;
 import java.util.List;
 
+@CrossOrigin (origins = "http://127.0.0.1:5500")
 @RestController
 public class CustomerController {
 
